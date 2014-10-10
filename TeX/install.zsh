b698f38a41f4a5db701d38f0d3d972b6569b29c0
Caleb Logemann <caleb.logemann@relationshipone.com>
2014-10-10 10:02:09 -0500

    updated install script to delete old link if there

--- TeX/install.zsh
+++ TeX/install.zsh
@@ -1,4 +1,10 @@
 #!#!/usr/bin/env zsh
+
+# delete old link if exists
+if [ -h "$HOME/.texmf" ]; then
+    rm "$HOME/.texmf"
+fi
+
 # link texmf to TEXMFHOME 
 TEX_ROOT=$(pwd);
 ln -s "$TEX_ROOT/texmf" "$HOME/.texmf"
