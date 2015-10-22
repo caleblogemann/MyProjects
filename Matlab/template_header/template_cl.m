08bb2e69f6b93fa554ff84e2b24af75fdcc3cb0a
Caleb Logemann <logemann@iastate.edu>
2015-10-22 16:10:08 -0500

    Added template for documenting Matlab functions

---
+++ Matlab/template_header/template_cl.m
@@ -0,0 +1,43 @@
+function [output1,output2] = function_name(input1,input2,input3)
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Author: Caleb Logemann
+% email: logemann@iastate.edu
+% Website: http://www.logemann.public.iastate.edu/
+% October 2015; Last revision: 22-October-2015
+
+%------------- BEGIN CODE --------------
+
+Enter your executable matlab commands here
+
+%------------- END OF CODE --------------
+%Please send suggestions for improvement of the above template header 
+%to Denis Gilbert at this email address: gilbertd@dfo-mpo.gc.ca.
+%Your contribution towards improving this template will be acknowledged in
+%the "Changes" section of the TEMPLATE_HEADER web page on the Matlab
+%Central File Exchange
