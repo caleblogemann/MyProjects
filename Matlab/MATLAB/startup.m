1cda47e6f8b75886b260da1299eb34b14261e011
Caleb Logemann <caleb.logemann@gmail.com>
2015-12-09 23:29:40 -0600

    Added folders to path in startup

---
+++ Matlab/MATLAB/startup.m
@@ -0,0 +1,6 @@
+% add toolbox directory to path
+addpath(genpath('/Users/caleblogemann/Documents/MATLAB/toolbox'));
+
+% add Research directory to path
+addpath(genpath('/Users/caleblogemann/Documents/Education/Undergraduate/Mathematics/Research'));
+rmpath(genpath('/Users/caleblogemann/Documents/Education/Undergraduate/Mathematics/Research/.git'));
